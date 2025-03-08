c269da80eaf5975840cde191492baac93ece38e7
pinkman <muhammedfaizan1999@gmail.com>
2025-03-08 17:18:07 +0530

    fun added from new branch

--- main.c
+++ main.c
@@ -2,6 +2,9 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
+
+void sample(void);
+
 int  main()
 {
 	printf("Hello world...\n");
@@ -9,5 +12,12 @@ int  main()
 	printf("Have a nic day...\n");
 	printf("How is the josh...\n");
 	printf("You are nice...\n");
+	sample();
 	return 0;
+}
+
+
+void sample(void)
+{
+	printf("sample fun from new...\n");
 }
